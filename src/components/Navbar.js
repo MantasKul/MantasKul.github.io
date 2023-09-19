3cd9c11388be8472f6fd586a0c4d25f9a7fbe4b3
Mantas <m.kulikauskas01@gmail.com>
2023-09-19 11:49:36 +0300

    Add botstrap style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 import Switch from 'react-switch';
 import React from "react";
 
+import './Navbar.css';
+
 export const Navbar = ({themeToggle, theme}) => {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <div class="navbar-nav">
-                    <Link class="nav-link active" aria-current="page" to="/">Home</Link>
-                    <Link class="nav-link" to="/about">About</Link>
-
-                    <div className="switch">
-                        <Switch onChange={ themeToggle } checked={ theme === "dark" } offColor="#bb8fce" onColor="#000000" uncheckedIcon="" checkedIcon=""/>
-                    </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/about">About</Link>
+                        </li>                    
+                        <li class="nav-item">
+                            <div className="switch">
+                                <Switch onChange={ themeToggle } checked={ theme === "dark" } offColor="#bb8fce" onColor="#000000" uncheckedIcon="" checkedIcon=""/>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
